9283c2e54e56ca2b1cd95d7a9c6ee2146854e8f1
Pranav Dixit <prnvdixit9@gmail.com>
2024-03-10 21:51:49 +0530

    Update Setting_bit_on_given_pos.c

--- C/Bit-Manipulation/Setting_bit_on_given_pos.c
+++ C/Bit-Manipulation/Setting_bit_on_given_pos.c
@@ -4,17 +4,17 @@ int main()
 {
     int pos, num;
     
-    printf("Enter the num: ");
+    printf("Enter the num: ");    // 4 in binary -> 0100
     scanf("%d", &num);
     
-    printf("Enter the Pos: ");
+    printf("Enter the Pos: ");    // 2nd position
     scanf("%d", &pos);
     
-    int n = 1 << (pos - 1);
+    int n = 1 << (pos - 1);    // 1 << (2-1)  ->  1 << 1  ->  0010
     
-    int ans = n | num;
+    int ans = n | num;        // 0100 | 0010  ->  0110
     
-    printf("Ans: %d", ans);
+    printf("Ans: %d", ans);    // 0110 in decimal -> 6
 
     return 0;
 }
